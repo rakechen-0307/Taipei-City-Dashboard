ff34567ef70ca82b3abe276bb2b7b7db4c5f6488
dienruei123 <dienruei@gmail.com>
2024-05-29 02:45:59 +0800

    Remove ws

--- Taipei-City-Dashboard-FE/src/App.vue
+++ Taipei-City-Dashboard-FE/src/App.vue
@@ -59,7 +59,7 @@ onBeforeMount(() => {
 		let vh = window.innerHeight * 0.01;
 		document.documentElement.style.setProperty("--vh", `${vh}px`);
 	});
-	contentStore.wsConnect();
+	// contentStore.wsConnect();
 });
 onMounted(() => {
 	const showInitialWarning = localStorage.getItem("initialWarning");
@@ -73,7 +73,7 @@ onMounted(() => {
 onBeforeUnmount(() => {
 	clearInterval(reloadChartData);
 	clearInterval(updateTimeToUpdate);
-	contentStore.wsDisconnect();
+	// contentStore.wsDisconnect();
 });
 </script>
 
